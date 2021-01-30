615b7aa6cfe975dd2fc6af53d2c7dfcc7daf668c
Kevin Perkins <kjperkin@users.noreply.github.com>
2021-01-29 23:26:09 -0800

    tools: compare_packages.sh: Add some variables to improve readability

--- tools/compare_packages.sh
+++ tools/compare_packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-if ! test $(command -v vimdiff 2> /dev/null); then
+: ${TEMPDIR:=${TEMPDIR}}
+: ${PKGS_TXT:=${PKGS_TXT}}
+: ${PKGS_AVAIL_TXT:=${PKGS_AVAIL_TXT}}
+: ${PKGS_TO_CHECK_TXT:=${PKGS_TO_CHECK_TXT}}
+: ${PKGS_ALL_TXT:=${PKGS_ALL_TXT}}
+
+if ! [[ `type -t vimdiff` =~ alias|file ]]; then
   echo "vimdiff not found."
   exit 1
 fi
@@ -7,15 +13,15 @@ if [ ! -f packages.yaml ]; then
   echo "packages.yaml not found."
   exit 1
 fi
-ls ../packages/*.rb | sort | cut -d'/' -f3 | cut -d'.' -f1 > /tmp/packages_all.txt
-rm -f /tmp/packages.txt
-touch /tmp/packages.txt
-for p in $(cat /tmp/packages_all.txt); do
-  if [ ! $(grep 'is_fake' ../packages/${p}.rb 2> /dev/null) ]; then
-    echo $p >> /tmp/packages.txt
+ls ../packages/*.rb | sort | cut -d'/' -f3 | cut -d'.' -f1 >"$PKGS_ALL_TXT"
+rm -f "$PKGS_TXT"
+touch "$PKGS_TXT"
+for p in $(cat "$PKGS_ALL_TXT"); do
+  if [ ! $(grep 'is_fake' ../packages/${p}.rb 2>/dev/null) ]; then
+    echo $p >> "$PKGS_TXT"
   fi
 done
-grep "^name:" packages.yaml | sort | cut -d' ' -f2 > /tmp/packages_to_check.txt
-sort < /tmp/packages.txt > /tmp/packages_available.txt
-vimdiff /tmp/packages_available.txt /tmp/packages_to_check.txt
-rm -f /tmp/packages.txt /tmp/packages_all.txt /tmp/packages_available.txt /tmp/packages_to_check.txt
+grep "^name:" packages.yaml | sort | cut -d' ' -f2 >"$PKGS_TO_CHECK_TXT"
+sort <"$PKGS_TXT" >"$PKGS_AVAIL_TXT"
+vimdiff ${PKGS_AVAIL_TXT} ${PKGS_TO_CHECK_TXT}
+rm -f ${PKGS_TXT} ${PKGS_ALL_TXT} ${PKGS_AVAIL_TXT} ${PKGS_TO_CHECK_TXT}
