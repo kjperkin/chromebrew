537ad03554501cd4a9b814c973f6e8ad56324009
Kevin Perkins <kjperkin@users.noreply.github.com>
2021-01-29 23:58:18 -0800

    tools: build.sh: use awk instead of grep and sed

--- tools/build.sh
+++ tools/build.sh
@@ -7,7 +7,10 @@ set -o pipefail
 # yes | crew install buildessential $keep
 arch=`uname -m`
 for i in "$@"; do
-    version=`grep '\<version ['"'"'"]' /usr/local/lib/crew/packages/$i.rb | head -1 | sed -e 's/^.*version *['"'"'"]//' -e 's/['"'"'"].*$//'`
+    version=$(awk -f <(cat <<EOF
+        /<version *['"]/ { sub(/^.*version *['"]/, ""); sub(/['"].*$/, ""); print; exit; }
+    EOF
+    ) <"${i}.rb"
     yes | crew build $i $keep 2>&1 | tee "$i-$version-chromeos-$arch.log"
     case .$? in
     .141) ;;        # ignore SIGPIPE
